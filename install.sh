14c5fceb6bd645b9dda987916a2d24bf7e9c1a62
mmcnair91 <62910185+mmcnair91@users.noreply.github.com>
2024-05-26 22:01:14 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,6 +19,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+#Removing previous installation attempts of Waveshare Installs
+rm -r  /Waveshare-DSI-LCD/
+
 # Step 1: Clone Waveshare-DSI-LCD driver repo
 echo "Cloning Waveshare-DSI-LCD driver repository..."
 cd ~
